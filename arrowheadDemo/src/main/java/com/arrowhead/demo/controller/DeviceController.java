f1c1d583e867a206e274eaa8bcaf8bb4bd9c20ba
Can <candeliorman@gmail.com>
2021-03-30 17:32:06 +0200

    added cors header

--- arrowheadDemo/src/main/java/com/arrowhead/demo/controller/DeviceController.java
+++ arrowheadDemo/src/main/java/com/arrowhead/demo/controller/DeviceController.java
@@ -3,10 +3,7 @@ package com.arrowhead.demo.controller;
 import com.arrowhead.demo.model.Device;
 import com.arrowhead.demo.repository.DeviceRepository;
 import com.arrowhead.demo.service.DeviceService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.Calendar;
@@ -23,12 +20,14 @@ public class DeviceController {
         this.service = service;
     }
 
+    @CrossOrigin
     @GetMapping("/device")
     List<Device> all() {
         //TODO this should also update the status of the devices. for this it should contact the devices.
         return service.updateStatus();
     }
 
+    @CrossOrigin
     @PostMapping("/device")
     Device newDevice(@RequestBody Device newDevice) {
         newDevice.setLast_notification_date(new Timestamp(Calendar.getInstance().getTimeInMillis()));
